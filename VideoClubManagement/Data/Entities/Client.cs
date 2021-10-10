7b8be472d3a0ebe36c9f3698a772f88555c551a9
Ronald Ogando <ronaldogando150@gmail.com>
2021-10-10 18:09:52 -0400

    Add Client and LegalPersonType Entities

---
+++ VideoClubManagement/Data/Entities/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace VideoClubManagement.Data.Entities
+{
+    public class Client : BaseEntity<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string TaxpayerIdentificationNumber { get; set; }
+        public string CreditCardNumber { get; set; }
+        public decimal CreditLimit { get; set; }
+    }
+}
---
+++ VideoClubManagement/Data/Enums/LegalPersonTypeId.cs
@@ -0,0 +1,8 @@
+﻿namespace VideoClubManagement.Data.Enums
+{
+    public enum LegalPersonTypeId : byte
+    {
+        Physical = 1,
+        Juridic = 2
+    }
+}
