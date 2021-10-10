f8c669ef1f76dc8cb5956a818b76f4201aa41ce6
Ronald Ogando <ronaldogando150@gmail.com>
2021-10-10 13:04:23 -0400

    Add CreatedDate and LastUpdatedDate Tracker to ApplicationDbContext .

--- VideoClubManagement/Data/ApplicationDbContext.cs
+++ VideoClubManagement/Data/ApplicationDbContext.cs
@@ -1,4 +1,9 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using VideoClubManagement.Data.Entities;
 
 namespace VideoClubManagement.Data
 {
@@ -9,5 +14,40 @@ namespace VideoClubManagement.Data
         {
         }
 
+        public override int SaveChanges()
+        {
+            UpdateEntriesEntities();
+            return base.SaveChanges();
+        }
+
+        public override async Task<int> SaveChangesAsync()
+        {
+            UpdateEntriesEntities();
+            return await base.SaveChangesAsync();
+        }
+
+        private void UpdateEntriesEntities()
+        {
+            var changeSet = ChangeTracker.Entries<BaseEntity>();
+
+            if (changeSet != null)
+            {
+                foreach (var entry in changeSet.Where(c => c.State != EntityState.Unchanged))
+                {
+                    switch (entry.State)
+                    {
+                        case EntityState.Added:
+                            entry.Entity.CreatedDate = DateTime.Now;
+                            entry.Entity.LastUpdatedDate = DateTime.Now;
+                            break;
+
+                        case EntityState.Modified:
+                            entry.Property(x => x.CreatedDate).IsModified = false;
+                            entry.Entity.LastUpdatedDate = DateTime.Now;
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
