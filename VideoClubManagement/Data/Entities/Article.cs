59a4d8413d9f9c10f8acee003466720a23daa705
Julio Sanchez <juliosanchez692@gmail.com>
2021-10-12 00:17:27 -0400

    Article class and reference.

---
+++ VideoClubManagement/Data/Entities/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace VideoClubManagement.Data.Entities
+{
+    class Article : BaseEntity<int>
+    {
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public ArticleType ArticleType { get; set; }
+        public Language Language { get; set; }
+        public decimal RentPerDay{ get; set; }
+        public int DaysRented { get; set; }
+        public decimal LateReturnFee { get; set; }
+    }
+}
