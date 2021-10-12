e8471b629521f1e99ed9a155083668cfe106abc4
Julio Sanchez <juliosanchez692@gmail.com>
2021-10-12 00:43:39 -0400

    Fixed Composition and naming convention

--- VideoClubManagement/Data/Entities/Article.cs
+++ VideoClubManagement/Data/Entities/Article.cs
@@ -4,8 +4,11 @@ namespace VideoClubManagement.Data.Entities
     class Article : BaseEntity<int>
     {
         public string Title { get; set; }
-        public Genre Genre { get; set; }
-        public ArticleType ArticleType { get; set; }
+        public int GenreId { get; set; }
+        public Genre Genres { get; set; }
+        public int ArticleTypeId { get; set; }
+        public ArticleType ArticleTypes { get; set; }
+        public int LanguageId { get; set; }
         public Language Language { get; set; }
         public decimal RentPerDay{ get; set; }
         public int DaysRented { get; set; }
