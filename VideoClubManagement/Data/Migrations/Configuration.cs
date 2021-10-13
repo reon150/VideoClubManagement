69202ba237d467768176f718e4bb06a1dd50ac9b
Ronald Ogando <ronaldogando150@gmail.com>
2021-10-12 22:50:25 -0400

    Add LegalPersonType Seed Data

--- VideoClubManagement/Data/Migrations/Configuration.cs
+++ VideoClubManagement/Data/Migrations/Configuration.cs
@@ -4,8 +4,10 @@
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using VideoClubManagement.Data.Entities;
+    using VideoClubManagement.Data.Enums;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<VideoClubManagement.Data.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
@@ -13,9 +15,26 @@
             MigrationsDirectory = @"Data\Migrations";
         }
 
-        protected override void Seed(VideoClubManagement.Data.ApplicationDbContext context)
+        protected override void Seed(ApplicationDbContext context)
         {
+            context.LegalPersonTypes.AddOrUpdate(
+                lp => lp.Id,
+                new LegalPersonType
+                {
+                    Id = LegalPersonTypeId.Juridic,
+                    Name = nameof(LegalPersonTypeId.Juridic),
+                    Description = "It is a non-human legal entity that is not a single natural person but an organization recognized by law as a legal person such as a corporation, government agency, or NGO.",
+                    IsActive = true
+                },
+                new LegalPersonType
+                {
+                    Id = LegalPersonTypeId.Physical,
+                    Name = nameof(LegalPersonTypeId.Physical),
+                    Description = "It is a person (in legal meaning, i.e., one who has its own legal personality) that is an individual human being.",
+                    IsActive = true
+                });
 
+             context.SaveChanges();
         }
     }
 }
