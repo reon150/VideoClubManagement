ffc46bce180b3053258227492896203463a54386
ncampusano <nancycampusano1@gmail.com>
2021-10-16 22:27:43 -0400

    UI design of Cast, Language and Employee

---
+++ VideoClubManagement/UI/Cast/castForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace VideoClubManagement.UI.Cast
+{
+    partial class castForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchPanel = new System.Windows.Forms.Panel();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.actionPanel = new System.Windows.Forms.Panel();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.dataPanel = new System.Windows.Forms.Panel();
+            this.castDataGridView = new System.Windows.Forms.DataGridView();
+            this.insertPanel = new System.Windows.Forms.Panel();
+            this.languageLabel = new System.Windows.Forms.Label();
+            this.lastnameTextBox = new System.Windows.Forms.TextBox();
+            this.lastnameLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.searchPanel.SuspendLayout();
+            this.actionPanel.SuspendLayout();
+            this.dataPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.castDataGridView)).BeginInit();
+            this.insertPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // searchPanel
+            // 
+            this.searchPanel.Controls.Add(this.searchTextBox);
+            this.searchPanel.Controls.Add(this.searchButton);
+            this.searchPanel.Location = new System.Drawing.Point(388, 12);
+            this.searchPanel.Name = "searchPanel";
+            this.searchPanel.Size = new System.Drawing.Size(372, 66);
+            this.searchPanel.TabIndex = 11;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(5, 28);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(236, 20);
+            this.searchTextBox.TabIndex = 19;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(247, 26);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 3;
+            this.searchButton.Text = "Buscar";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // actionPanel
+            // 
+            this.actionPanel.Controls.Add(this.deleteButton);
+            this.actionPanel.Controls.Add(this.updateButton);
+            this.actionPanel.Controls.Add(this.addButton);
+            this.actionPanel.Location = new System.Drawing.Point(9, 169);
+            this.actionPanel.Name = "actionPanel";
+            this.actionPanel.Size = new System.Drawing.Size(366, 66);
+            this.actionPanel.TabIndex = 10;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(226, 0);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 2;
+            this.deleteButton.Text = "Eliminar";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(145, 0);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 1;
+            this.updateButton.Text = "Actualizar";
+            this.updateButton.UseVisualStyleBackColor = true;
+            // 
+            // addButton
+            // 
+            this.addButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.addButton.Location = new System.Drawing.Point(64, 0);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Agregar";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // dataPanel
+            // 
+            this.dataPanel.Controls.Add(this.castDataGridView);
+            this.dataPanel.Location = new System.Drawing.Point(388, 84);
+            this.dataPanel.Name = "dataPanel";
+            this.dataPanel.Size = new System.Drawing.Size(372, 155);
+            this.dataPanel.TabIndex = 9;
+            // 
+            // castDataGridView
+            // 
+            this.castDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.castDataGridView.ColumnHeadersVisible = false;
+            this.castDataGridView.Location = new System.Drawing.Point(5, 3);
+            this.castDataGridView.Name = "castDataGridView";
+            this.castDataGridView.Size = new System.Drawing.Size(360, 149);
+            this.castDataGridView.TabIndex = 0;
+            // 
+            // insertPanel
+            // 
+            this.insertPanel.Controls.Add(this.languageLabel);
+            this.insertPanel.Controls.Add(this.lastnameTextBox);
+            this.insertPanel.Controls.Add(this.lastnameLabel);
+            this.insertPanel.Controls.Add(this.nameTextBox);
+            this.insertPanel.Controls.Add(this.nameLabel);
+            this.insertPanel.Location = new System.Drawing.Point(9, 25);
+            this.insertPanel.Name = "insertPanel";
+            this.insertPanel.Size = new System.Drawing.Size(366, 138);
+            this.insertPanel.TabIndex = 8;
+            // 
+            // languageLabel
+            // 
+            this.languageLabel.AutoSize = true;
+            this.languageLabel.Location = new System.Drawing.Point(118, 14);
+            this.languageLabel.Name = "languageLabel";
+            this.languageLabel.Size = new System.Drawing.Size(93, 13);
+            this.languageLabel.TabIndex = 18;
+            this.languageLabel.Text = "Gestión de elenco";
+            // 
+            // lastnameTextBox
+            // 
+            this.lastnameTextBox.Location = new System.Drawing.Point(99, 90);
+            this.lastnameTextBox.Name = "lastnameTextBox";
+            this.lastnameTextBox.Size = new System.Drawing.Size(236, 20);
+            this.lastnameTextBox.TabIndex = 7;
+            // 
+            // lastnameLabel
+            // 
+            this.lastnameLabel.AutoSize = true;
+            this.lastnameLabel.Location = new System.Drawing.Point(43, 93);
+            this.lastnameLabel.Name = "lastnameLabel";
+            this.lastnameLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lastnameLabel.Size = new System.Drawing.Size(50, 13);
+            this.lastnameLabel.TabIndex = 6;
+            this.lastnameLabel.Text = "Apellido: ";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(99, 55);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(236, 20);
+            this.nameTextBox.TabIndex = 5;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(43, 58);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(50, 13);
+            this.nameLabel.TabIndex = 4;
+            this.nameLabel.Text = "Nombre: ";
+            // 
+            // castForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 256);
+            this.Controls.Add(this.searchPanel);
+            this.Controls.Add(this.actionPanel);
+            this.Controls.Add(this.dataPanel);
+            this.Controls.Add(this.insertPanel);
+            this.Name = "castForm";
+            this.Text = "Gestión de elenco";
+            this.searchPanel.ResumeLayout(false);
+            this.searchPanel.PerformLayout();
+            this.actionPanel.ResumeLayout(false);
+            this.dataPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.castDataGridView)).EndInit();
+            this.insertPanel.ResumeLayout(false);
+            this.insertPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel searchPanel;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Panel actionPanel;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Panel dataPanel;
+        private System.Windows.Forms.DataGridView castDataGridView;
+        private System.Windows.Forms.Panel insertPanel;
+        private System.Windows.Forms.Label languageLabel;
+        private System.Windows.Forms.TextBox lastnameTextBox;
+        private System.Windows.Forms.Label lastnameLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label nameLabel;
+    }
+}
---
+++ VideoClubManagement/UI/Language/languageForm.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace VideoClubManagement.UI.Language
+{
+    partial class languageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchPanel = new System.Windows.Forms.Panel();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.actionPanel = new System.Windows.Forms.Panel();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.dataPanel = new System.Windows.Forms.Panel();
+            this.languageDataGridView = new System.Windows.Forms.DataGridView();
+            this.insertPanel = new System.Windows.Forms.Panel();
+            this.languageLabel = new System.Windows.Forms.Label();
+            this.langCodeTextBox = new System.Windows.Forms.TextBox();
+            this.lastnameLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.searchPanel.SuspendLayout();
+            this.actionPanel.SuspendLayout();
+            this.dataPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.languageDataGridView)).BeginInit();
+            this.insertPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // searchPanel
+            // 
+            this.searchPanel.Controls.Add(this.searchTextBox);
+            this.searchPanel.Controls.Add(this.searchButton);
+            this.searchPanel.Location = new System.Drawing.Point(391, 12);
+            this.searchPanel.Name = "searchPanel";
+            this.searchPanel.Size = new System.Drawing.Size(372, 66);
+            this.searchPanel.TabIndex = 7;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(5, 28);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(236, 20);
+            this.searchTextBox.TabIndex = 19;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(247, 26);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 3;
+            this.searchButton.Text = "Buscar";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // actionPanel
+            // 
+            this.actionPanel.Controls.Add(this.deleteButton);
+            this.actionPanel.Controls.Add(this.updateButton);
+            this.actionPanel.Controls.Add(this.addButton);
+            this.actionPanel.Location = new System.Drawing.Point(12, 169);
+            this.actionPanel.Name = "actionPanel";
+            this.actionPanel.Size = new System.Drawing.Size(366, 66);
+            this.actionPanel.TabIndex = 6;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(226, 0);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 2;
+            this.deleteButton.Text = "Eliminar";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(145, 0);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 1;
+            this.updateButton.Text = "Actualizar";
+            this.updateButton.UseVisualStyleBackColor = true;
+            // 
+            // addButton
+            // 
+            this.addButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.addButton.Location = new System.Drawing.Point(64, 0);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Agregar";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // dataPanel
+            // 
+            this.dataPanel.Controls.Add(this.languageDataGridView);
+            this.dataPanel.Location = new System.Drawing.Point(391, 84);
+            this.dataPanel.Name = "dataPanel";
+            this.dataPanel.Size = new System.Drawing.Size(372, 155);
+            this.dataPanel.TabIndex = 5;
+            // 
+            // languageDataGridView
+            // 
+            this.languageDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.languageDataGridView.ColumnHeadersVisible = false;
+            this.languageDataGridView.Location = new System.Drawing.Point(5, 3);
+            this.languageDataGridView.Name = "languageDataGridView";
+            this.languageDataGridView.Size = new System.Drawing.Size(360, 149);
+            this.languageDataGridView.TabIndex = 0;
+            // 
+            // insertPanel
+            // 
+            this.insertPanel.Controls.Add(this.languageLabel);
+            this.insertPanel.Controls.Add(this.langCodeTextBox);
+            this.insertPanel.Controls.Add(this.lastnameLabel);
+            this.insertPanel.Controls.Add(this.nameTextBox);
+            this.insertPanel.Controls.Add(this.nameLabel);
+            this.insertPanel.Location = new System.Drawing.Point(12, 25);
+            this.insertPanel.Name = "insertPanel";
+            this.insertPanel.Size = new System.Drawing.Size(366, 138);
+            this.insertPanel.TabIndex = 4;
+            // 
+            // languageLabel
+            // 
+            this.languageLabel.AutoSize = true;
+            this.languageLabel.Location = new System.Drawing.Point(118, 14);
+            this.languageLabel.Name = "languageLabel";
+            this.languageLabel.Size = new System.Drawing.Size(96, 13);
+            this.languageLabel.TabIndex = 18;
+            this.languageLabel.Text = "Gestión de idiomas";
+            // 
+            // langCodeTextBox
+            // 
+            this.langCodeTextBox.Location = new System.Drawing.Point(99, 90);
+            this.langCodeTextBox.Name = "langCodeTextBox";
+            this.langCodeTextBox.Size = new System.Drawing.Size(236, 20);
+            this.langCodeTextBox.TabIndex = 7;
+            // 
+            // lastnameLabel
+            // 
+            this.lastnameLabel.AutoSize = true;
+            this.lastnameLabel.Location = new System.Drawing.Point(20, 93);
+            this.lastnameLabel.Name = "lastnameLabel";
+            this.lastnameLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lastnameLabel.Size = new System.Drawing.Size(73, 13);
+            this.lastnameLabel.TabIndex = 6;
+            this.lastnameLabel.Text = "Código idioma";
+            this.lastnameLabel.Click += new System.EventHandler(this.lastnameLabel_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(99, 55);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(236, 20);
+            this.nameTextBox.TabIndex = 5;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(43, 58);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(50, 13);
+            this.nameLabel.TabIndex = 4;
+            this.nameLabel.Text = "Nombre: ";
+            // 
+            // languageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 259);
+            this.Controls.Add(this.searchPanel);
+            this.Controls.Add(this.actionPanel);
+            this.Controls.Add(this.dataPanel);
+            this.Controls.Add(this.insertPanel);
+            this.Name = "languageForm";
+            this.Text = "Gestión de idiomas";
+            this.searchPanel.ResumeLayout(false);
+            this.searchPanel.PerformLayout();
+            this.actionPanel.ResumeLayout(false);
+            this.dataPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.languageDataGridView)).EndInit();
+            this.insertPanel.ResumeLayout(false);
+            this.insertPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel searchPanel;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Panel actionPanel;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Panel dataPanel;
+        private System.Windows.Forms.DataGridView languageDataGridView;
+        private System.Windows.Forms.Panel insertPanel;
+        private System.Windows.Forms.Label languageLabel;
+        private System.Windows.Forms.TextBox langCodeTextBox;
+        private System.Windows.Forms.Label lastnameLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label nameLabel;
+    }
+}
