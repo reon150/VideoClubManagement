030890c292b113ad07371094fe8f3ce03ed4a717
Julio Sanchez <juliosanchez692@gmail.com>
2021-11-14 10:10:16 -0400

    Added validations for Genre, ArticleType and Articles. Fixed reverted commitments.

---
+++ VideoClubManagement/Validations/ArticleTypeValidator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using VideoClubManagement.Data.Entities;
+
+namespace VideoClubManagement.Validations
+{
+    public class ArticleTypeValidator : IValidator<ArticleType>
+    {
+        private readonly IDbSet<ArticleType> _dbContext;
+
+        public ArticleTypeValidator(IDbSet<ArticleType> dbContext) => _dbContext = dbContext;
+
+        public List<string> GetValidationErrors(ArticleType entity)
+        {
+            List<string> errors = new List<string>();
+
+            if (string.IsNullOrWhiteSpace(entity.Name))
+                errors.Add("El nombre no puede estar vacio.");
+            else if (entity.Name.Length > 100)
+                errors.Add("La longitud del nombre no puede ser mayor a 100.");
+
+            if (string.IsNullOrWhiteSpace(entity.Description))
+                errors.Add("La descripció no puede estar vacía.");
+            else if (entity.Description.Length > 100)
+                errors.Add("La longitud de la descripción no puede ser mayor a 100.");
+
+            return errors;
+        }
+    }
+}
---
+++ VideoClubManagement/Validations/GenreValidator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using VideoClubManagement.Data.Entities;
+
+namespace VideoClubManagement.Validations
+{
+    public class GenreValidator : IValidator<Genre>
+    {
+        private readonly IDbSet<Genre> _dbContext;
+
+        public GenreValidator(IDbSet<Genre> dbContext) => _dbContext = dbContext;
+
+        public List<string> GetValidationErrors(Genre entity)
+        {
+            List<string> errors = new List<string>();
+
+            if (string.IsNullOrWhiteSpace(entity.Name))
+                errors.Add("El nombre no puede estar vacío.");
+            else if (entity.Name.Length > 100)
+                errors.Add("La longitud del nombre no puede ser mayor a 100.");
+
+            if (string.IsNullOrWhiteSpace(entity.Description))
+                errors.Add("La descripción no puede estar vacía.");
+            else if (entity.Description.Length > 100)
+                errors.Add("La longitud de la descripción no puede ser mayor a 100.");
+
+            return errors;
+        }
+    }
+}
