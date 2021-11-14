769054de6c6777c6bd7bf112328283e063f485e5
ncampusano <nancycampusano1@gmail.com>
2021-11-14 00:32:46 -0400

    Validations to entities and some improvements to functionality.

--- VideoClubManagement/UI/Cast/castForm.cs
+++ VideoClubManagement/UI/Cast/castForm.cs
@@ -3,6 +3,8 @@ using System.Data;
 using System.Linq;
 using System.Windows.Forms;
 using VideoClubManagement.Data;
+using VideoClubManagement.Data.Entities;
+using VideoClubManagement.Validations;
 
 namespace VideoClubManagement.UI.Cast
 {
@@ -11,17 +13,26 @@ namespace VideoClubManagement.UI.Cast
         public Data.Entities.Cast Casts { get; set; }
         ApplicationDbContext applicationDbContext = new ApplicationDbContext();
         private readonly Form _parent;
+        private IValidator<Data.Entities.Cast> _validator;
 
         public castForm(Form parent)
         {
             InitializeComponent();
             _parent = parent;
+            _validator = new CastValidator(applicationDbContext.Casts);
+
         }
         private void refreshData()
         {
            castDataGridView.DataSource = applicationDbContext.Casts.ToList();
         }
 
+        private void clearData()
+        {
+            nameTextBox.Clear();
+            lastnameTextBox.Clear();
+            idTextBox.Clear();
+        }
         private void generalSearch()
         {
             var Casts = from sh in applicationDbContext.Casts
@@ -47,12 +58,34 @@ namespace VideoClubManagement.UI.Cast
         {
             try
             {
+                var save = MessageBox.Show($"¿Estás seguro que deseas guardar estos datos?",
+               "Pregunta", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK;
+                if (save)
+                {
+                    Data.Entities.Cast cast = new Data.Entities.Cast
+                    {
+                        FirstName = nameTextBox.Text,
+                        LastName = lastnameTextBox.Text
+                    };
+                    var validationErrors = _validator.GetValidationErrors(cast);
+                    if (validationErrors != null && validationErrors.Count > 0)
+                    {
+                        string errors = "";
+                        foreach (var validationError in validationErrors)
+                            errors += $"{ validationError }{ Environment.NewLine }";
 
-                applicationDbContext.Casts.Add(new Data.Entities.Cast { FirstName = nameTextBox.Text, LastName = lastnameTextBox.Text });
-                applicationDbContext.SaveChanges();
-                MessageBox.Show("El registro se guardo con exito");
-                refreshData();
+                        MessageBox.Show(errors, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        applicationDbContext.Casts.Add(cast);
+                        applicationDbContext.SaveChanges();
+                        MessageBox.Show("El registro se guardo con exito");
+                        refreshData();
+                        clearData();
 
+                    }
+                }
             }
             catch (Exception ex)
             {
@@ -66,17 +99,35 @@ namespace VideoClubManagement.UI.Cast
         {
             try
             {
+                var update = MessageBox.Show($"¿Estás seguro que deseas actualizar estos datos?",
+               "Pregunta", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK;
                 Data.Entities.Cast cast = applicationDbContext.Casts.Find(Int32.Parse(idTextBox.Text));
-                if (cast != null)
+                cast.FirstName = nameTextBox.Text;
+                cast.LastName = lastnameTextBox.Text;
+                if (update)
                 {
+                    if (cast != null)
+                    {
+                        var validationErrors = _validator.GetValidationErrors(cast);
+                        if (validationErrors != null && validationErrors.Count > 0)
+                        {
+                            string errors = "";
+                            foreach (var validationError in validationErrors)
+                                errors += $"{ validationError }{ Environment.NewLine }";
 
-                    cast.FirstName = nameTextBox.Text;
-                    cast.LastName = lastnameTextBox.Text;
-
-
+                            MessageBox.Show(errors, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else
+                        {
+                            applicationDbContext.SaveChanges();
+                            MessageBox.Show("Registro actualizado con exito.");
+                            refreshData();
+                            clearData();
+                        }
+                       
+                    }
+                   
                 }
-                MessageBox.Show("Registro actualizado con exito.");
-                refreshData();
             }
             catch (Exception ex)
             {
---
+++ VideoClubManagement/Validations/LanguageValidator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoClubManagement.Data.Entities;
+
+namespace VideoClubManagement.Validations
+{
+    class LanguageValidator : IValidator<Language>
+    {
+        private readonly IDbSet<Language> _dbContext;
+
+        public LanguageValidator(IDbSet<Language> dbContext) => _dbContext = dbContext;
+
+        public List<string> GetValidationErrors(Language entity)
+        {
+            List<string> errors = new List<string>();
+            if (string.IsNullOrWhiteSpace(entity.Description))
+                errors.Add("El nombre no puede estar vacio.");
+            else if (entity.Description.Length > 100)
+                errors.Add("La longitud del nombre no puede ser mayor a 100.");
+            if (string.IsNullOrWhiteSpace(entity.ISOCode))
+                errors.Add("El nombre no puede estar vacio.");
+            else if (entity.ISOCode.Length > 3)
+                errors.Add("La longitud del codigo de idioma no puede ser mayor a 3.");
+            if (!string.IsNullOrWhiteSpace(entity.ISOCode))
+            {
+                if (_dbContext.FirstOrDefault(x => x.ISOCode == entity.ISOCode) != null)
+                    errors.Add("El codigo de idioma se encuentra registrado en el sistema.");
+            }
+            return errors;
+
+
+        }
+
+        public object GetValidationErrors(object cast)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
